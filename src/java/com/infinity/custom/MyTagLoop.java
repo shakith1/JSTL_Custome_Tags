446c6a99d0d164b27f4d5f5cac12b88ebbf1e11b
Shakith <nimsara.shakith@gmail.com>
2022-10-13 11:39:07 +0530

    Custom Tag Loop Tag

---
+++ src/java/com/infinity/custom/MyTagLoop.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.infinity.custom;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+/**
+ *
+ * @author Shakith
+ */
+public class MyTagLoop extends TagSupport{
+
+    @Override
+    public int doStartTag() throws JspException {
+        
+    }
+    
+}
